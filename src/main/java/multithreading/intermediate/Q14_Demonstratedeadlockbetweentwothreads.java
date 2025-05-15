2ad329e2f043320a6a1fbc928d8176fb7d635376
Durgesh Konga <kongadurgesh20@gmail.com>
2025-05-15 20:38:02 +0530

    2nd commit

--- src/main/java/multithreading/intermediate/Q14_Demonstratedeadlockbetweentwothreads.java
+++ src/main/java/multithreading/intermediate/Q14_Demonstratedeadlockbetweentwothreads.java
@@ -1,7 +1,42 @@
 package multithreading.intermediate;
 
-public class Q14 {
-    public static void main(String[] args) {
-        // TODO: Demonstrate deadlock between two threads
+public class Q14_Demonstratedeadlockbetweentwothreads {
+    public static void main(String[] args) throws InterruptedException {
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("Lock 1 acquired by Thread 1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("Lock 2 acquired by Thread 1");
+                }
+            }
+
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("Lock 2 acquired by Thread 2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    System.out.println("Lock 1 acquired by Thread 2");
+                }
+            }
+
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
     }
 }
--- src/main/java/multithreading/intermediate/Q18_Createthreadsafesingletonusingsynchronization.java
+++ src/main/java/multithreading/intermediate/Q18_Createthreadsafesingletonusingsynchronization.java
@@ -1,6 +1,6 @@
 package multithreading.intermediate;
 
-public class Q18 {
+public class Q18_Createthreadsafesingletonusingsynchronization {
     public static void main(String[] args) {
         // TODO: Create thread-safe singleton using synchronization
     }
