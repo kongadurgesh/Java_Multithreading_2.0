6e78e26abc14949283e827db18fd523685d24e98
Durgesh Konga <kongadurgesh20@gmail.com>
2025-05-15 22:24:54 +0530

    4th commit - completed till intermediate questions

--- src/main/java/multithreading/intermediate/Q19_UseReentrantLockinsteadofsynchronizedblock.java
+++ src/main/java/multithreading/intermediate/Q19_UseReentrantLockinsteadofsynchronizedblock.java
@@ -1,7 +1,45 @@
 package multithreading.intermediate;
 
-public class Q19 {
-    public static void main(String[] args) {
-        // TODO: Use ReentrantLock instead of synchronized block
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Q19_UseReentrantLockinsteadofsynchronizedblock {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLockExample example = new ReentrantLockExample();
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                example.increment();
+            }
+        };
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        System.out.println("Counter: " + example.getCounter());
+    }
+}
+
+class ReentrantLockExample {
+    private ReentrantLock lock = new ReentrantLock();
+    private int counter = 0;
+
+    public void increment() {
+        lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getCounter() {
+        lock.lock();
+        try {
+            return counter;
+        } finally {
+            lock.unlock();
+        }
     }
 }
--- src/main/java/multithreading/intermediate/Q20_UsetryLockwithtimeout.java
+++ src/main/java/multithreading/intermediate/Q20_UsetryLockwithtimeout.java
@@ -1,7 +1,53 @@
 package multithreading.intermediate;
 
-public class Q20 {
-    public static void main(String[] args) {
-        // TODO: Use tryLock with timeout
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Q20_UsetryLockwithtimeout {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLockExample1 counter = new ReentrantLockExample1();
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    counter.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        System.out.println("Counter: " + counter.getCounter());
+    }
+}
+
+class ReentrantLockExample1 {
+    private ReentrantLock lock = new ReentrantLock();
+    private int counter = 0;
+
+    public void increment() throws InterruptedException {
+        if (lock.tryLock(1L, TimeUnit.SECONDS)) {
+            try {
+                counter++;
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public int getCounter() throws InterruptedException {
+        if (lock.tryLock(1L, TimeUnit.SECONDS)) {
+            try {
+                return counter;
+            } finally {
+                lock.unlock();
+            }
+        }
+        return -1;
     }
 }
